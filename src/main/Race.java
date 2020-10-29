05a885853efd544a965451afc19f094a639df99e
pragya <pragya.sharma2_cs19@gla.ac.in>
2020-10-29 19:52:06 +0530

    Create Object of Tortoise class and start the Tortoise thread.

--- src/main/Race.java
+++ src/main/Race.java
@@ -7,5 +7,7 @@ public class Race {
     public static void main(String[] args) {
         Rabbit rabbit = new Rabbit();
         rabbit.getRabbit().start();
+        Tortoise tortoise = new Tortoise();
+        tortoise.start();
     }
 }
