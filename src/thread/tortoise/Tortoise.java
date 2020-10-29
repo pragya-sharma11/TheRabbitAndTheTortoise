fd849a62008ed9dbc568869d5c1e17d6d0554647
pragya <pragya.sharma2_cs19@gla.ac.in>
2020-10-29 19:43:02 +0530

    Create run() and make switch cases statement for milestones.

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -5,4 +5,25 @@ public class Tortoise extends Thread {
         super("Tortoise");
     }
     private final int MILESTONE=5;
+    public void run() {
+        for (int index = 0; index < MILESTONE; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortiose is at Start Line");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise is at Cool River");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise is at Mountain Hills");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise is at Oak tree");
+                    break;
+                case 4:
+                    System.out.println("The Tortoise is at Finish line");
+                    break;
+            }
+        }
+    }
 }
